96919037cf8c22517cc9c5347879d218217ce419
Himadri Mandal <himadri1800@gmail.com>
2019-07-02 00:55:28 +0530

    Added question module

--- src/main/java/com/himadri/school/questionnaire/domain/QuestionModule.java
+++ src/main/java/com/himadri/school/questionnaire/domain/QuestionModule.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.ZonedDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Himadri
@@ -14,7 +17,7 @@ import javax.persistence.*;
 @Setter
 @Data
 @Entity
-@Table(name = "question_module")
+@Table(name = "module")
 public class QuestionModule {
 
     @Id
@@ -24,4 +27,28 @@ public class QuestionModule {
     private String moduleName;
 
     private String description;
+
+    @OneToMany
+    @JoinTable(
+            name = "module_question",
+            joinColumns = @JoinColumn(name = "module_id"),
+            inverseJoinColumns = @JoinColumn(name = "question_id")
+    )
+    private Set<Question> questions = new HashSet<>();
+
+    private ZonedDateTime createdOn;
+
+    private ZonedDateTime UpdatedOn;
+
+    @PrePersist
+    public void prePersist(){
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        this.setCreatedOn(zonedDateTime);
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        this.setUpdatedOn(zonedDateTime);
+    }
 }
